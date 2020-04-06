be9f3426d014351d1224e895043981421f6951bb
juanjovn <juanjovn@gmail.com>
2020-04-06 20:23:37 +0200

    Add custom colors extension

---
+++ Task-Shuffler/Utils/CustomColors.swift
@@ -0,0 +1,17 @@
+//
+//  CustomColors.swift
+//  Task-Shuffler
+//
+//  Created by Juanjo Valiño on 06/04/2020.
+//  Copyright © 2020 Juanjo Valiño. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static var mysticBlue: UIColor { return #colorLiteral(red: 0.1333333333, green: 0.2196078431, blue: 0.262745098, alpha: 1) }
+    static var pearlWhite: UIColor { return #colorLiteral(red: 0.937254902, green: 0.9450980392, blue: 0.9529411765, alpha: 1) }
+    static var fireOrange: UIColor { return #colorLiteral(red: 0.9137254902, green: 0.3098039216, blue: 0.2156862745, alpha: 1) }
+    static var naturGreen: UIColor { return #colorLiteral(red: 0.7921568627, green: 0.7921568627, blue: 0.6666666667, alpha: 0.8543343322) }
+    static var powerGreen: UIColor { return #colorLiteral(red: 0.1254901961, green: 0.7490196078, blue: 0.4196078431, alpha: 1) }
+}
