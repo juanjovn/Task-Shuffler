c19721bc65c3025caafad4e1c52e7ff1feb4e724
Juanjo Valiño <juanjovn@gmail.com>
2020-12-31 19:43:31 +0100

    Add Shuffle Modes model

---
+++ Task-Shuffler/Shuffle/Model/ShuffleModes.swift
@@ -0,0 +1,14 @@
+//
+//  ShuffleModes.swift
+//  Task-Shuffler
+//
+//  Created by Juanjo Valiño on 31/12/20.
+//  Copyright © 2020 Juanjo Valiño. All rights reserved.
+//
+
+import Foundation
+
+class ShuffleModes {
+    static let howModes = ["Smart", "Random", "Single"]
+    static let whenModes = ["All", "This", "Next"]
+}
