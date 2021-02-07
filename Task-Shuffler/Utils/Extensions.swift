2927ce0d145910db0928584f46d2fd36e78ab4a9
Juanjo Valiño <juanjovn@gmail.com>
2021-02-07 23:50:03 +0100

    Separate extensions in a file

---
+++ Task-Shuffler/Utils/Extensions.swift
@@ -0,0 +1,147 @@
+//
+//  Extensions.swift
+//  Task-Shuffler
+//
+//  Created by Juanjo Valiño on 7/2/21.
+//  Copyright © 2021 Juanjo Valiño. All rights reserved.
+//
+
+import UIKit
+
+extension Calendar {
+    var currentDate: Date { return Date() }
+    static let gregorian = Calendar(identifier: .gregorian)
+    
+    func isDateInNextWeek(_ date: Date) -> Bool {
+        guard let nextWeek = self.date(byAdding: DateComponents(weekOfYear: 1), to: currentDate) else {
+            return false
+        }
+        return isDate(date, equalTo: nextWeek, toGranularity: .weekOfYear)
+    }
+}
+
+extension Date {
+    func startOfWeek(using calendar: Calendar = .gregorian) -> Date {
+        calendar.dateComponents([.calendar, .yearForWeekOfYear, .weekOfYear], from: self).date!
+    }
+    
+    
+    static func today() -> Date {
+        let cal = NSCalendar.current
+        let components = cal.dateComponents([.year, .month, .day], from: Date())
+        let today = cal.date(from: components)
+        return today!
+    }
+    
+    func addDays(_ days: Int) -> Date? {
+        let dayComponenet = NSDateComponents()
+        dayComponenet.day = days
+        let theCalendar = NSCalendar.current
+        let nextDate = theCalendar.date(byAdding: dayComponenet as DateComponents, to: self)
+        return nextDate
+    }
+}
+
+extension UIColor {
+    var rgba: (red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat) {
+        var red: CGFloat = 0
+        var green: CGFloat = 0
+        var blue: CGFloat = 0
+        var alpha: CGFloat = 0
+        getRed(&red, green: &green, blue: &blue, alpha: &alpha)
+        
+        return (red, green, blue, alpha)
+    }
+}
+
+extension UIImageView {
+    static func fromGif(frame: CGRect, resourceName: String) -> UIImageView? {
+        guard let path = Bundle.main.path(forResource: resourceName, ofType: "gif") else {
+            print("Gif does not exist at that path")
+            return nil
+        }
+        let url = URL(fileURLWithPath: path)
+        guard let gifData = try? Data(contentsOf: url),
+              let source =  CGImageSourceCreateWithData(gifData as CFData, nil) else { return nil }
+        var images = [UIImage]()
+        let imageCount = CGImageSourceGetCount(source)
+        for i in 0 ..< imageCount {
+            if let image = CGImageSourceCreateImageAtIndex(source, i, nil) {
+                images.append(UIImage(cgImage: image))
+            }
+        }
+        let gifImageView = UIImageView(frame: frame)
+        gifImageView.animationImages = images
+        return gifImageView
+    }
+}
+
+extension UIImage {
+    func tinted(color: UIColor) -> UIImage? {
+        let image = self.withRenderingMode(.alwaysTemplate)
+        let imageView = UIImageView(image: image)
+        imageView.tintColor = color
+        
+        UIGraphicsBeginImageContext(image.size)
+        if let context = UIGraphicsGetCurrentContext() {
+            imageView.layer.render(in: context)
+            let tintedImage = UIGraphicsGetImageFromCurrentImageContext()
+            UIGraphicsEndImageContext()
+            return tintedImage
+        } else {
+            return self
+        }
+    }
+    
+    func addingShadow(blur: CGFloat = 1, shadowColor: UIColor = UIColor(white: 0, alpha: 1), offset: CGSize = CGSize(width: 1, height: 1) ) -> UIImage {
+        
+        UIGraphicsBeginImageContextWithOptions(CGSize(width: size.width + 2 * blur, height: size.height + 2 * blur), false, 0)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        context.setShadow(offset: offset, blur: blur, color: shadowColor.cgColor)
+        draw(in: CGRect(x: blur - offset.width / 2, y: blur - offset.height / 2, width: size.width, height: size.height))
+        let image = UIGraphicsGetImageFromCurrentImageContext()!
+        
+        UIGraphicsEndImageContext()
+        
+        return image
+    }
+}
+
+extension CALayer {
+    func fit(rect: CGRect) {
+        frame = rect
+        
+        sublayers?.forEach { $0.fit(rect: rect) }
+    }
+}
+
+extension UIView {
+    func fitLayers() {
+        layer.fit(rect: bounds)
+    }
+}
+
+extension String {
+    
+    func withBoldText(text: String, font: UIFont? = nil) -> NSMutableAttributedString {
+        let _font = font ?? UIFont.systemFont(ofSize: 14, weight: .regular)
+        let fullString = NSMutableAttributedString(string: self, attributes: [NSAttributedString.Key.font: _font])
+        let boldFontAttribute: [NSAttributedString.Key: Any] = [NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: _font.pointSize)]
+        let range = (self as NSString).range(of: text)
+        fullString.addAttributes(boldFontAttribute, range: range)
+        return fullString
+    }
+    
+}
+
+//Just for mute a autolayout warning by a bug of iOS: https://stackoverflow.com/questions/55653187/swift-default-alertviewcontroller-breaking-constraints
+extension UIAlertController {
+    func pruneNegativeWidthConstraints() {
+        for subView in self.view.subviews {
+            for constraint in subView.constraints where constraint.debugDescription.contains("width == - 16") {
+                subView.removeConstraint(constraint)
+            }
+        }
+    }
+}
